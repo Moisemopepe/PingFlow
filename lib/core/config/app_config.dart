1b131ed29ec1d9e7ffd19bf062419a9ee7a1a985
MOPEPE MOISE <moisemopepe3@gmail.com>
2026-05-02 19:06:26 +0100

    Use public PingFlow backend

--- lib/core/config/app_config.dart
+++ lib/core/config/app_config.dart
@@ -3,6 +3,6 @@ class AppConfig {
 
   static const backendBaseUrl = String.fromEnvironment(
     'PINGFLOW_API_BASE_URL',
-    defaultValue: 'http://10.0.2.2:8787',
+    defaultValue: 'https://pingflow-api.onrender.com',
   );
 }
